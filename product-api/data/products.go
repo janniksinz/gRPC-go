57e879f0ed4ea3ad19b5aa74ebf7d5b9a08dc911
janniksinz <j.sinz@gmx.de>
2024-07-08 16:05:23 +0200

    feat: product data validation

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -3,20 +3,22 @@ package data
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/go-playground/validator"
 	"io"
+	"regexp"
 	"time"
 )
 
 // Product defines the structure for an API product
 type Product struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float32 `json:"price"`
-	SKU         string  `json:"sku"`
-	CreatedOn   string  `json:"-"`
-	UpdatedOn   string  `json:"-"`
-	DeletedOn   string  `json:"-"`
+	ID          int     `json:"id" validate:""`
+	Name        string  `json:"name" validate:"required"`
+	Description string  `json:"description" validate:""`
+	Price       float32 `json:"price" validate:"gt=0"`
+	SKU         string  `json:"sku" validate:"required,sku"`
+	CreatedOn   string  `json:"-" validate:""`
+	UpdatedOn   string  `json:"-" validate:""`
+	DeletedOn   string  `json:"-" validate:""`
 }
 
 func (p *Product) FromJSON(r io.Reader) error {
@@ -77,10 +79,29 @@ func getNextID() int {
 	return lp.ID + 1
 }
 
+// Validate
+func (p *Product) Validate() error {
+	validate := validator.New()
+	validate.RegisterValidation("sku", validateSKU)
+	return validate.Struct(p) // returns error package
+}
+
+func validateSKU(fl validator.FieldLevel) bool {
+	// sku is of format abc-abcd-abcdef
+	re := regexp.MustCompile(`[a-z]+-[a-z]+-[a-z]+`)
+	matches := re.FindAllString(fl.Field().String(), -1) // -1 returns all matches
+
+	if len(matches) != 1 {
+		return false
+	}
+
+	return true
+}
+
 // productList is a hard coded list of products for this
 // example data source
 var productList = []*Product{
-	&Product{
+	{
 		ID:          1,
 		Name:        "Latte",
 		Description: "Frothy milky coffee",
@@ -89,7 +110,7 @@ var productList = []*Product{
 		CreatedOn:   time.Now().UTC().String(),
 		UpdatedOn:   time.Now().UTC().String(),
 	},
-	&Product{
+	{
 		ID:          2,
 		Name:        "Espresso",
 		Description: "Short and strong coffee without milk",
